1809b9a2ad19a720a95abd8eda85e25b0742596e
LtdLar <94563153+LtdLar@users.noreply.github.com>
2023-04-28 19:54:17 +0200

    Update Account.c
    CreateAccount()

--- Account.c
+++ Account.c
@@ -16,14 +16,14 @@ void CreateAccount(int a){
     int id;
     FILE* file;
     
-    printf("Votre nom ?\n");
+    printf("Votre nom ?\n");    //Prise des données d'identification
     scanf("%s",&name);
     printf("Votre nom de Famille ?");
     scanf("%s",&lastname);
     printf("Bienvenue chez Umid Inc. %s %s\n",name,lastname^);
    
     id= rand() %1000; //On suppose qu'on a moins de 1000 clients
-    file=fopen(("%s",name),"w");
+    file=fopen(("%s",name),"w");  //création du fichier récapitulatif 
     fprintf(file,"id = %d",id);
     fprintf("name = %s",name);
     fclose(file);
@@ -32,7 +32,7 @@ void CreateAccount(int a){
 
 int main(){
     
-    CreateAccount(1);
+    CreateAccount(1);  //pour l'instant on garde le paramètre de la procédure on le dégagera plus tard
     
     return 0;
 }
