fc141a70aab0365b493cffa31b8958415497ad00
Ocenyna <131042956+Ocenyna@users.noreply.github.com>
2023-05-27 16:36:59 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -13,99 +13,61 @@ typedef struct {
     int fidelite;
 } Client;
 
-typedef struct {
-    char taille[3];// TROIS TAILLE A DEFINIR 
-    float prix; // A DEFINIR (PEUT ETRE INUTILE)
-    int quantite; // TRES IMPORTANT A DEFINIR 
-} TeeShirt;
+struct shirt{
+    char *name;
+    int num;
+    int stocklevel;
+    float price;
+    char size;
+};
 
-Client clients[MAX_CLIENTS];
-int numClients = 0;
-TeeShirt stock[MAX_STOCK];
-int numArticles = 0;
-
-void creerCompteClient() {
-    Client client;
-
-    printf("Création d'un compte client\n");
-    printf("Nom : ");
-    scanf("%s", client.nom);
-    printf("Prénom : ");
-    scanf("%s", client.prenom);
-    printf("Code client : ");
-    scanf("%d", &client.codeClient);
-    printf("Compte de fidélité (0: Non, 1: Oui) : ");
-    scanf("%d", &client.fidelite);
-
-    clients[numClients++] = client;
-
-    printf("Compte client créé avec succès.\n");
+void EmptyBuffer(){
+    while(getchar()!='\n');
 }
-
-int trouverClient(int codeClient) {
-    int i;
-    for (i = 0; i < numClients; i++) {
-        if (clients[i].codeClient == codeClient) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-// FAIRE CAS OU COMPTE N'EXISTE PAS 
-
-void achat(Client *client) {
-    int codeClient;
-    int indexClient;
-
-    printf("Achat de tee-shirt\n");
-    printf("Code client : ");
-    scanf("%d", &codeClient);
-
-    indexClient = trouverClient(codeClient);
-
-// FONCTIONNE PAS 
     
-    
-    
-}
-
-void gestionMagasin() {
-    int choix;
-
-    printf("Gestion du magasin\n");
-    printf("1. Voir le stock\n");
-    printf("2. Ajouter du stock\n");
-    printf("3. Supprimer du stock\n");
-    printf("0. Quitter\n");
-    printf("Choix : ");
-    scanf("%d", &choix);
-
-    switch (choix) {
-        case 0:
-            printf("Au revoir !\n");
-            exit(0);
-        case 1:
-            // fonction afficher le stock
-            break;
-        case 2:
-            //fonction ajouter du stock
-            break;
-        case 3:
-            // fonction supprimer du stock
-            break;
-        default:
-            printf("Choix invalide.\n");
-    }
-}
 
 int main() {
-    int mode;
+    int mode, b;
     int codeClient;
     int indexClient;
     Client client;
 
     printf("Bienvenue dans le magasin de tee-shirts !\n");
+    do{
+    printf("Avez-vous un compte ? Tapez 1 si oui et 2 si non.\n");
+    scanf("%d", &b);
+    vide_buffer();
+    } while (b!=1 && b!=2);
+    if(b==1){
+    do{
+    printf("Entrez votre ID: ");
+    scanf("%d",&id);
+    EmptyBuffer();
+    }while (id =! 26 && id =! 958);
+    }
+    if(b==2){
+        CreateAccount();
+    }
+    printf("Choisissez le mode :\n");
+    printf("1. Achat\n");
+    printf("2. Supprimer mon compte\n");
+    printf("0. Quitter\n");
+    printf("Choix : ");
+    scanf("%d", &mode);
+    switch (choix) {
+        case 0:
+            printf("Au revoir !\n");
+            exit(0);
+        case 1:
+            buy(purple, red, blue, green);
+            break;
+        case 2:
+            DeleteAccount();
+            break;
+        default:
+            printf("Choix invalide.\n");
+            Clientmode();
+    }
 
     printf("Choisissez le mode :\n");
     printf("1. Gestion du magasin\n");
@@ -114,6 +76,18 @@ int main() {
     printf("Choix : ");
     scanf("%d", &mode);
 
-    switch (mode);
+    switch (mode){
+        case 0:
+            printf("Au revoir !\n");
+            exit(0);
+        case 1:
+            
+            break;
+        case 2:
+            //fonction ajouter du stock
+            break;
+        default:
+            printf("Choix invalide.\n");
+            main();
     }
     
